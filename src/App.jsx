2c0e7755a6c647d65392ad6f0a93a26625fc1646
AbsharHassan <abshar.hassan7@gmail.com>
2023-02-14 04:22:36 +0500

    fixed search error, added suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from './components/ui/Header'
 import BackgroundImage from './components/ui/BackgroundImage'
 import CharacterGrid from './components/characters/CharacterGrid'
 import Search from './components/ui/Search'
 import './App.css'
-
 
 const App = () => {
   const [items, setItems] = useState([])
@@ -14,26 +13,33 @@ const App = () => {
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://thronesapi.com/api/v2/Characters/${query}`)
+      // Returns an array when no query param but returns an object when query param present
+      // Also this should be in a try-catch block because it will throw an error if the id (query param) does not exist in their database. Discuss this with me in person.
+      const result = await axios(
+        `https://thronesapi.com/api/v2/Characters/${query}`
+      )
 
-      console.log(query);
-      console.log(result.data)
+      // commented out these to prevent the console from being flooded and becoming harder to debug
+      // console.log(query)
+      // console.log(result.data)
+
       setItems(result.data)
       setIsLoading(false)
     }
     fetchItems()
   }, [query])
 
-
   return (
     <>
-    <div className="container"> 
-      <Header/>
-      <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid isLoading={isLoading} items={items} />
-      <BackgroundImage/>
-    </div>
-    
+      <div className="container">
+        <Header />
+        <Search getQuery={(q) => setQuery(q)} />
+        <CharacterGrid
+          isLoading={isLoading}
+          items={items}
+        />
+        <BackgroundImage />
+      </div>
     </>
   )
 }
--- src/components/characters/CharacterGrid.jsx
+++ src/components/characters/CharacterGrid.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import { useEffect } from 'react'
 import CharacterItem from './CharacterItem'
 import Spinner from '../ui/Spinner'
 
+// Main error is in this component. When you make a call to the api with an id param, it returns an object, NOT an array. Your version of this component will only work if the items prop is an array. (the .map() method only works on arrays), to fix this, you need to first check if the items prop is an array, if it is, render it how you are currently rendering it. If it is not an array, then render a single <CharacterItem ... />  component
 
-const CharacterGrid = ({items, isLoading}) => {
-  items = Array.from(items)
+// use the Array.isArray() method to check if items prop is an array or not
+
+const CharacterGrid = ({ items, isLoading }) => {
+  // items = Array.from(items) ---> DO NOT OVERWRITE PROPS. SUPPOSED TO BE READ ONLY
+
+  // It is always a good practice to use the useEffect hook to see the value of props or state whenever they change in development. Delete this useEffect hook in production/when you are done with this component
+  useEffect(() => {
+    console.log(items) // notice how first this is an array, but when you enter something in search, it changes to an object
+  }, [items])
+
   return isLoading ? (
-  <Spinner/>
+    <Spinner />
   ) : (
-  <section className='cards'>
-    {items.map((item) => (
-         <CharacterItem key={item.id} item={item} />
-    ))}
-  </section>
-  
+    <section className="cards">
+      {Array.isArray(items) ? ( // checks if items is an array or not
+        items.map(
+          (
+            item // rendered ONLY when items prop is an array
+          ) => (
+            <CharacterItem
+              key={item.id}
+              item={item}
+            />
+          )
+        )
+      ) : (
+        <CharacterItem // rendered ONLY when items prop is NOT an array
+          key={items.id}
+          item={items}
+        />
+      )}
+    </section>
   )
 }
 
 export default CharacterGrid
-
- 
